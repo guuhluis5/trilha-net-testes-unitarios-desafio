dc7b0cb6ed9e67d3e03ef7d418bef717bb42a1d3
guuhluis5 <gustavosantista012@gmail.com>
2023-12-31 17:37:49 -0300

    Ultimas modificações em testes unitários

--- TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
@@ -45,9 +45,12 @@ public class ValidacoesListaTests
 
         // Act
 
-        // Assert
-    }
+        var resultado = _validacoes.ListaContemDeterminadoNumero(lista, numeroParaProcurar);
 
+        // Assert
+        Assert.False(resultado);
+    }
+    [Fact]
     //TODO: Corrigir a anotação [Fact]
     public void DeveMultiplicarOsElementosDaListaPor2()
     {
@@ -58,8 +61,11 @@ public class ValidacoesListaTests
         var resultadoEsperado = new List<int> { 10, 14, 16, 18 };
         
         // Act
+        var resultado = _validacoes.MultiplicarNumerosLista(lista, 2);
+
 
         // Assert
+        Assert.Equal(resultado, resultadoEsperado);
     }
 
     [Fact]
@@ -71,10 +77,11 @@ public class ValidacoesListaTests
         var lista = new List<int> { 5, -1, -8, 9 };
 
         // Act
+        var resultado = _validacoes.RetornarMaiorNumeroLista(lista);
 
         // Assert
         //TODO: Corrigir o Assert.Equal com base no retorno da chamada ao método
-        Assert.Equal(9, 9);
+        Assert.Equal(9, resultado);
     }
 
     [Fact]
@@ -90,6 +97,6 @@ public class ValidacoesListaTests
 
         // Assert
         //TODO: Corrigir o Assert.Equal com base no retorno da chamada ao método
-        Assert.Equal(-8, -8);
+        Assert.Equal(-8, resultado);
     }
 }
